bf9bdf7db66af8ea03bf0b265820a50a5d09e8ed
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-02-25 11:24:58 +0700

    Implement cart functionality and integrate Tailwind CSS for styling

--- my-solidjs-app/src/App.jsx
+++ my-solidjs-app/src/App.jsx
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
 import Card from './components/Card';
+import { createSignal } from 'solid-js';
 
 function App() {
   const data = [1, 2, 3]
+  const [cart, setCart] = createSignal(0)
   return (
     <div className={styles.App}>
       <header>
         <h1>Ninja Merch</h1>
       </header>
-      <img src={logo} alt="logo" style={{ width: '100px', height: 'auto', margin: '20px auto' }} />
+      <img src={logo} alt="logo" class='w-5 h-5' />
+      <button onClick={() => setCart(cart() + 1)}>Add to cart</button>
+      {cart()}
       <div>
         {data.map((item) => (
           <Card></Card>
